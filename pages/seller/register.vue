72acf736bf2fabb711b24728bab430fb3afdd1bc
bondsanti <64140204+bondsanti@users.noreply.github.com>
2021-07-10 19:12:16 +0700

    register + navber

---
+++ pages/seller/register.vue
@@ -0,0 +1,289 @@
+<template>
+  <v-container style="background-color:#ededed;height:100%" fluid>
+         
+  <v-card 
+    class="mx-auto mt-6 py-3"
+    elevaation="5"
+    justify-centaer>
+    <v-card-title>
+            <v-dialog
+            v-model="dialog"
+            max-width="500px"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                color="primary"
+                dark
+                class="mr-5"
+                v-bind="attrs"
+                v-on="on"
+              >
+                <v-icon left> mdi-account</v-icon> ลงทะเบียนสมาชิก
+              </v-btn>
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="text-h5"><v-icon left> mdi-account-box </v-icon> ข้อมูลลูกค้า</span>
+              </v-card-title>
+  
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col cols="12">
+                    <v-select
+              label="คำนำหน้า"
+              outlined
+              
+              color="#1D1D1D"
+            :items="items"
+            ></v-select>
+                    </v-col>
+                    <v-col
+                      cols="12"
+                         md="6"
+                      class="mt-n7"
+                    >
+                       <v-text-field
+              outlined
+              label="ชื่อ"
+              required
+              color="#1D1D1D"
+             
+            ></v-text-field>
+                    </v-col>
+                    <v-col
+                      cols="12"
+                       md="6"
+                       class="mt-n7"
+                    >
+
+            <v-text-field
+              outlined
+              label="นามสกุล"
+              required
+              color="#1D1D1D"
+         
+            ></v-text-field>
+    
+                    </v-col>
+<v-col cols="12"  class="mt-n7">
+            <v-text-field
+              outlined
+              label="วันเกิด"
+              required
+              color="#1D1D1D"
+         
+            ></v-text-field>
+                    </v-col>
+          
+<v-col cols="12"  class="mt-n7">
+            <v-text-field
+              outlined
+              label="เบอร์โทร"
+              required
+              color="#1D1D1D"
+         
+            ></v-text-field>
+                    </v-col>
+                    <v-col cols="12"  class="mt-n7">
+            <v-text-field
+              outlined
+              label="อีเมล์"
+              required
+              color="#1D1D1D"
+         
+            ></v-text-field>
+                    </v-col>
+                     <v-col cols="12"  class="mt-n7">
+  <v-textarea
+              color="#1D1D1D"
+              outlined
+              name=""
+              label="ที่อยู่"
+             
+            ></v-textarea>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+  
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="close"
+                >
+                  Cancel
+                </v-btn>
+                <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="save"
+                >
+                  Save
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+      <v-spacer></v-spacer>
+
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      :search="search"
+    ></v-data-table>
+  </v-card>
+  
+  </v-container>
+</template>
+
+<script>
+import milkShake from "@/assets/milkshake.svg";
+export default {
+    data(){
+        return {
+          search: '',
+          items: ['นาย', 'นาง', 'นางสาว'],
+          headers: [
+          {
+            text: 'Dessert (100g serving)',
+            align: 'start',
+            sortable: false,
+            value: 'name',
+          },
+          { text: 'Calories', value: 'calories' },
+          { text: 'Fat (g)', value: 'fat' },
+          { text: 'Carbs (g)', value: 'carbs' },
+          { text: 'Protein (g)', value: 'protein' },
+          { text: 'Iron (%)', value: 'iron' },
+        ],
+         desserts: [
+          {
+            name: 'Frozen Yogurt',
+            calories: 159,
+            fat: 6.0,
+            carbs: 24,
+            protein: 4.0,
+            iron: '1%',
+          },
+          {
+            name: 'Ice cream sandwich',
+            calories: 237,
+            fat: 9.0,
+            carbs: 37,
+            protein: 4.3,
+            iron: '1%',
+          },
+          {
+            name: 'Eclair',
+            calories: 262,
+            fat: 16.0,
+            carbs: 23,
+            protein: 6.0,
+            iron: '7%',
+          },
+          {
+            name: 'Cupcake',
+            calories: 305,
+            fat: 3.7,
+            carbs: 67,
+            protein: 4.3,
+            iron: '8%',
+          },
+          {
+            name: 'Gingerbread',
+            calories: 356,
+            fat: 16.0,
+            carbs: 49,
+            protein: 3.9,
+            iron: '16%',
+          },
+          {
+            name: 'Jelly bean',
+            calories: 375,
+            fat: 0.0,
+            carbs: 94,
+            protein: 0.0,
+            iron: '0%',
+          },
+          {
+            name: 'Lollipop',
+            calories: 392,
+            fat: 0.2,
+            carbs: 98,
+            protein: 0,
+            iron: '2%',
+          },
+          {
+            name: 'Honeycomb',
+            calories: 408,
+            fat: 3.2,
+            carbs: 87,
+            protein: 6.5,
+            iron: '45%',
+          },
+          {
+            name: 'Donut',
+            calories: 452,
+            fat: 25.0,
+            carbs: 51,
+            protein: 4.9,
+            iron: '22%',
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+            fat: 26.0,
+            carbs: 65,
+            protein: 7,
+            iron: '6%',
+          },
+        ],
+
+        }
+    },
+
+  watch: {
+    dialog (val) {
+      val || this.close()
+    },
+    dialogDelete (val) {
+      val || this.closeDelete()
+    },
+  },
+  components: {
+    milkShake
+  }
+};
+</script>
+
+<style>
+.scroll::-webkit-scrollbar {
+  width: 17px;
+}
+
+.scroll::-webkit-scrollbar-track {
+  background: #ededed;
+  border-left: 1px solid #ededed;
+  border-radius: 10px;
+}
+
+.scroll::-webkit-scrollbar-thumb {
+  background: #b0b0b0;
+  border: solid 3px #e6e6e6;
+  border-radius: 7px;
+}
+
+.scroll::-webkit-scrollbar-thumb:hover {
+  background: black;
+}
+</style>

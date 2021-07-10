845962e8c34d7576f61388f3001f7f6e83b76590
PAKPING_UP59 <48551951+pakping@users.noreply.github.com>
2021-07-10 13:08:26 +0700

    stock ui

---
+++ pages/manage/stock.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="mt-3">  
+  
+   <stock />
+   
+</div>
+
+</template>
+
+<script>
+import stock from '@/components/manage/stock.vue'
+
+export default {
+  components:{ 
+     stock ,
+   
+     }
+
+}
+</script>
+
+<style>
+
+</style>

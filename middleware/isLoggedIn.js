daa89c8cdbcd6ee12b9a915907d2bc716dac8e8c
James_Devfong <james@mail.com>
2021-07-13 22:50:55 +0700

    login bug else if

--- middleware/isLoggedIn.js
+++ middleware/isLoggedIn.js
@@ -1,5 +1,8 @@
 export default function (context) {
   if (context.store.state.auth.loggedIn) {
-    return context.redirect('/seller')
+    if (context.store.state.auth.user.ref_id_role.position !== 'cashier') {
+      return context.redirect('/member')
+    }
+    //return context.redirect('/seller')
   }
 }

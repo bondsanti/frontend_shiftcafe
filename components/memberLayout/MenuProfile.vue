4ba8d6580c359d0f381775e18cf0577261387125
bondsanti <64140204+bondsanti@users.noreply.github.com>
2021-07-13 20:26:32 +0700

    memberPage

--- components/memberLayout/MenuProfile.vue
+++ components/memberLayout/MenuProfile.vue
@@ -1,23 +1,21 @@
 <template>
-
-  <div class="my-5 ma-2">
+  <div class="">
     <div class="text-center mb-5">
-     
-        <v-img height="100%" src="classic.png">
-          <v-row align="end">
-            <v-col align-self="start" class="pa-6" cols="4">
-              <v-avatar class="profile" size="100">
-                <v-img src="https://cdn.vuetifyjs.com/images/john.jpg"></v-img>
-              </v-avatar>
-            </v-col>
-            <v-col align-self="start" class="pa-5 mt-10 " cols="8">
-              <v-list-item-subtitle class="white--text text-uppercase">
-                Mr. Santi Chooprayoon</v-list-item-subtitle
-              >
-            </v-col>
-          </v-row>
-        </v-img>
-      
+      <v-img height="100%" src="classic.png">
+        <v-row align="end">
+          <v-col align-self="start" class="pa-6" cols="4">
+            <v-avatar class="profile" size="100">
+              <v-img src="https://cdn.vuetifyjs.com/images/john.jpg"></v-img>
+            </v-avatar>
+          </v-col>
+          <v-col align-self="start" class="pa-5 mt-10 " cols="8">
+            <v-list-item-subtitle class="white--text text-uppercase">
+              Mr. Santi Chooprayoon</v-list-item-subtitle
+            >
+          </v-col>
+        </v-row>
+      </v-img>
+
       <!--<v-avatar size="180" style="border: 4px solid #ffffff">
         <img :src="account.avatar_img" />
       </v-avatar>-->
@@ -31,13 +29,13 @@
       </v-progress-linear>
     </div>
 
-    <v-card class="mx-auto">
+    <v-card class="mx-auto hidden-xs-and-up">
       <v-list flat>
         <h4 class="text-center pa-2">
           สวัสดี คุณ : <span class="red_fix--text"> Santi</span>
         </h4>
         <v-divider></v-divider>
-        <v-list-item-group v-model="selectedItem" color="red_fix">
+        <v-list-item-group v-model="selectedItem">
           <v-list-item v-for="(item, i) in items" :key="i" :to="item.to">
             <v-list-item-icon>
               <v-icon v-text="item.icon"></v-icon>
@@ -49,21 +47,44 @@
         </v-list-item-group>
       </v-list>
     </v-card>
+
+    <v-bottom-navigation
+      color="error"
+      fixed
+      class="hidden-lg-and-up"
+      shift
+      small
+    >
+      <v-btn to="/point">
+        <span>พอยท์</span>
+        <v-icon>mdi-gift</v-icon>
+      </v-btn>
+      <v-btn to="/coupon">
+        <span>คูปอง</span>
+        <v-icon>mdi-ticket-percent</v-icon>
+      </v-btn>
+      <v-btn to="/member" x-small>
+        <span>ข้อมูล</span>
+        <v-icon>mdi-account</v-icon>
+      </v-btn>
+      <v-btn to="/history_buy">
+        <span>ประวัติ</span>
+        <v-icon>mdi-history</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
   </div>
-
-
 </template>
 <script>
+import Footer from "../layout/Footer.vue";
 export default {
-   layout:"layoutMember",
+  components: { Footer },
+  layout: "layoutMember",
   data() {
     return {
       selectedItem: 0,
       knowledge: 33,
       items: [
-        { text: "ข้อมูลส่วนตัว", 
-        icon: "mdi-account",
-         to: "/member" },
+        { text: "ข้อมูลส่วนตัว", icon: "mdi-account", to: "/member" },
         {
           text: "พอยท์",
           icon: "mdi-gift",
@@ -71,13 +92,18 @@ export default {
         },
         {
           text: "คูปอง",
-          icon: "mdi-checkbook",
-          to: "/member/coupon"
+          icon: "mdi-ticket-percent",
+          to: "/coupon"
         },
         {
           text: "ประวัติการสั่งซื้อ",
           icon: "mdi-history",
-          to: "/member/history_buy"
+          to: "/history_buy"
+        },
+        {
+          text: "ออกระบบ",
+          icon: "mdi-logout",
+          to: "/logout"
         }
       ],
       account: {
---
+++ pages/history_buy/index.vue
@@ -0,0 +1,150 @@
+<template>
+  <div class="" style="height: 100%">
+    <v-card class="py-5 px-5" style="height: 100%;" color="secondary">
+      <v-row>
+        <v-col cols="12" xs="12" sm="12" md="3">
+          <MenuProfile />
+        </v-col>
+
+        <v-col xs="12" sm="12" md="9" class="">
+          <v-card class="px-6 py-5 mb-5">
+            <div class="text-center">
+              <h2 class="">รายการประวัติการสั่งซื้อ</h2>
+              <v-divider class="mt-3 mb-2"></v-divider>
+            </div>
+            <v-data-table
+              :headers="headers"
+              :items="pointItems"
+              :items-per-page="10"
+              class="mb-n5"
+            ></v-data-table>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import MenuProfile from "~/components/memberLayout/MenuProfile";
+export default {
+  layout: "layoutMember",
+  data() {
+    return {
+      headers: [
+        {
+          text: "หมายเลขบิล",
+          align: "start",
+          sortable: false,
+          value: "orderid"
+        },
+        { text: "วันที่", value: "datetime" },
+        { text: "ประเภทรายการ", value: "typeOrder" },
+        { text: "ส่วนลด", value: "discount" },
+        { text: "ยอดสิทธิ", value: "total_amout" }
+      ],
+      pointItems: [
+        {
+          orderid: "214001",
+          typeOrder: "ทานที่ร้าน",
+          discount: 0,
+          total_amout: 100,
+          datetime: "13/7/2564 13:00"
+        },
+        {
+          orderid: "214001",
+          typeOrder: "ทานที่ร้าน",
+          discount: 0,
+          total_amout: 100,
+          datetime: "13/7/2564 13:00"
+        },
+        {
+          orderid: "214001",
+          typeOrder: "ทานที่ร้าน",
+          discount: 0,
+          total_amout: 100,
+          datetime: "13/7/2564 13:00"
+        },
+        {
+          orderid: "214001",
+          typeOrder: "ทานที่ร้าน",
+          discount: 0,
+          total_amout: 100,
+          datetime: "13/7/2564 13:00"
+        },
+        {
+          orderid: "214001",
+          typeOrder: "ทานที่ร้าน",
+          discount: 0,
+          total_amout: 100,
+          datetime: "13/7/2564 13:00"
+        },
+        {
+          orderid: "214001",
+          typeOrder: "ทานที่ร้าน",
+          discount: 0,
+          total_amout: 100,
+          datetime: "13/7/2564 13:00"
+        },
+        {
+          orderid: "214001",
+          typeOrder: "ทานที่ร้าน",
+          discount: 0,
+          total_amout: 100,
+          datetime: "13/7/2564 13:00"
+        },
+        {
+          orderid: "214001",
+          typeOrder: "ทานที่ร้าน",
+          discount: 0,
+          total_amout: 100,
+          datetime: "13/7/2564 13:00"
+        },
+        {
+          orderid: "214001",
+          typeOrder: "ทานที่ร้าน",
+          discount: 0,
+          total_amout: 100,
+          datetime: "13/7/2564 13:00"
+        },
+        {
+          orderid: "214001",
+          typeOrder: "ทานที่ร้าน",
+          discount: 0,
+          total_amout: 100,
+          datetime: "13/7/2564 13:00"
+        },
+        {
+          orderid: "214001",
+          typeOrder: "ทานที่ร้าน",
+          discount: 0,
+          total_amout: 100,
+          datetime: "13/7/2564 13:00"
+        },
+        {
+          orderid: "214001",
+          typeOrder: "ทานที่ร้าน",
+          discount: 0,
+          total_amout: 100,
+          datetime: "13/7/2564 13:00"
+        }
+      ]
+    };
+  },
+
+  components: {
+    MenuProfile
+  }
+};
+</script>
+
+<style>
+.text-plus {
+  color: green;
+  font-size: 14px;
+}
+.text-minus {
+  color: red;
+  font-size: 14px;
+}
+</style>

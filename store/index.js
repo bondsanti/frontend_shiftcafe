47bc191049be4dfa95ba9db304c656437bdeede6
James_Devfong <james@mail.com>
2021-07-13 10:39:28 +0700

    login_bug

--- store/index.js
+++ store/index.js
@@ -1,3 +1,17 @@
 export const state = () => ({
-  title: 'SHIFT CAFE'
+  title: 'SHIFT CAFE',
+  displayName: 'Long2'
 })
+
+export const mutations = () => ({
+  setDisplayName (state, user) {
+    state.setDisplayName = user
+  }
+})
+
+export const getters = () => ({
+  displayName (state) {
+    return state.displayName
+  }
+})
+export const actions = () => ({})

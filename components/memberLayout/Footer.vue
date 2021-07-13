7d51ca4392dd5492a4f83f23b67147ac21ab3ee6
bondsanti <64140204+bondsanti@users.noreply.github.com>
2021-07-13 10:04:37 +0700

    footerbug

---
+++ components/memberLayout/Footer.vue
@@ -0,0 +1,48 @@
+<template>
+  <div> 
+    <v-bottom-navigation
+      :value="value"
+      color="error"
+      fixed
+      class="hidden-lg-and-up"
+      height="80px"
+      style="z-index: 1000"
+    >
+      <v-btn to="/members/history_buy">
+        <span>ประวัติ</span>
+
+        <v-icon>mdi-history</v-icon>
+      </v-btn>
+
+      <v-btn to="/dealer">
+        <span>ซื้อล็อตเตอรี่</span>
+
+        <v-icon>mdi-search</v-icon>
+      </v-btn>
+
+      <v-btn to="/complain">
+        <span>ร้องเรียน</span>
+
+        <v-icon>mdi-chat</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+      <v-footer absolute app dark color="#1d1d1d" fi>
+    <span>&copy; {{ new Date().getFullYear() }}</span>
+  </v-footer>
+
+  </div>
+</template>
+<script>
+
+export default {
+  data() {
+    return {
+      value: 2,
+      show_cart: false,
+      date_cart_exp: null,
+    }
+  },
+
+}
+</script>
+<style scoped></style>

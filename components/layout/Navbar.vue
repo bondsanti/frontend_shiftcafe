7ebe78731c2aa8a878ed15cd85ab71938091e41d
bondsanti <64140204+bondsanti@users.noreply.github.com>
2021-07-20 20:13:55 +0700

    add_orderPage

--- components/layout/Navbar.vue
+++ components/layout/Navbar.vue
@@ -19,35 +19,37 @@
         </v-list-item>
       </v-list>
       <!-- 1 -->
+      <template> </template>
 
       <!-- 2 -->
-          <template v-slot:append>
+      <template v-slot:append>
         <div class="pa-2">
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in below"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
+          <v-list>
+            <v-list-item
+              v-for="(item, i) in below"
+              :key="i"
+              :to="item.to"
+              router
+              exact
+            >
+              <v-list-item-action>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-item-action>
+
+              <v-list-item-content>
+                <v-list-item-title v-text="item.title" />
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
         </div>
-          </template>
+      </template>
       <!-- 2 -->
     </v-navigation-drawer>
     <v-app-bar :clipped-left="clipped" fixed app color="#1d1d1d" dark>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title>SHIFT CAFE</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn icon>
+      <!-- <v-btn icon>
         <v-icon>mdi-circle-box</v-icon>
       </v-btn>
       <v-btn icon>
@@ -55,14 +57,14 @@
       </v-btn>
       <v-btn icon>
         <v-icon>mdi-clipboard-list</v-icon>
-      </v-btn>
+      </v-btn> -->
+      <v-avatar size="36px" class="mr-2">
+        <img alt="Avatar" src="profile-user.png" />
+      </v-avatar>
+      <v-toolbar-title>{{ $store.getters["displayName"] }}</v-toolbar-title>
       <v-btn icon @click="logout">
         <v-icon>mdi-logout</v-icon>
       </v-btn>
-        <v-toolbar-title>{{ $store.getters["displayName"] }}</v-toolbar-title>
-      <v-avatar size="36px" class="ml-2">
-        <img alt="Avatar" src="profile-user.png" />
-      </v-avatar>
     </v-app-bar>
   </div>
 </template>
@@ -73,6 +75,48 @@ export default {
       clipped: false,
       drawer: false,
       fixed: false,
+      items2: [
+        {
+          action: "mdi-ticket",
+          items3: [{ title: "List Item" }],
+          title: "Attractions"
+        },
+        {
+          action: "mdi-silverware-fork-knife",
+          active: true,
+          items: [
+            { title: "Breakfast & brunch" },
+            { title: "New American" },
+            { title: "Sushi" }
+          ],
+          title: "Dining"
+        },
+        {
+          action: "mdi-school",
+          items: [{ title: "List Item" }],
+          title: "Education"
+        },
+        {
+          action: "mdi-run",
+          items: [{ title: "List Item" }],
+          title: "Family"
+        },
+        {
+          action: "mdi-bottle-tonic-plus",
+          items: [{ title: "List Item" }],
+          title: "Health"
+        },
+        {
+          action: "mdi-content-cut",
+          items: [{ title: "List Item" }],
+          title: "Office"
+        },
+        {
+          action: "mdi-tag",
+          items: [{ title: "List Item" }],
+          title: "Promotions"
+        }
+      ],
       below: [
         {
           icon: "mdi-cog ",
@@ -141,6 +185,11 @@ export default {
           icon: "mdi-ticket-percent-outline ",
           title: "จัดการคูปอง",
           to: "/manage/coupon"
+        },
+        {
+          icon: "mdi-note-text-outline",
+          title: "ข้อมูลการสั่งซื้อ",
+          to: "/manage/order"
         }
       ],
       miniVariant: false,

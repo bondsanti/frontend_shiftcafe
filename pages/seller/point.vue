a827b5e7fa2e9009a3beb92198430e734b03fd43
PAKPING_UP59 <48551951+pakping@users.noreply.github.com>
2021-07-20 20:17:24 +0700

    re

--- pages/seller/point.vue
+++ pages/seller/point.vue
@@ -1,14 +1,29 @@
 <template>
-  <pointmanage />
+  <pointmanage :pointmanage="pointmanage" />
 </template>
 
 <script>
 import pointmanage from "@/components/manage/pointmanage.vue";
 export default {
   layout: "layoutCashier",
+ middleware: ["auth", "check"],
+  async asyncData(context) {
+    const pointmanage = await context.$axios.$get("/point-manage");
+    //console.log(pointmanage);
+    return { pointmanage };
+  },
   components: {
     pointmanage
-  }
+  },
+  // methods: {
+  //   async addlevelmember(datalevelmember) {
+  //     await this.$axios.$post("/point-manage", datalevelmember);
+  //     this.pointmanage = await this.$axios.$get("/point-manage");
+  //   }
+  // },
+  data: () => ({
+    pointmanage: []
+  })
 };
 </script>
 

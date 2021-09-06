77212081f202c1aed63cfa47b57ebc602e02cb25
JA EM <james@devfong.co.th>
2021-09-06 20:20:50 +0700

    add_color_to_localstorage

--- middleware/refresh.js
+++ middleware/refresh.js
@@ -1,4 +1,21 @@
 export default function(context) {
+  const primary = localStorage.getItem("primary");
+  const success = localStorage.getItem("success");
+  const error = localStorage.getItem("error");
+  const warning = localStorage.getItem("warning");
+  const accent = localStorage.getItem("accent");
+  const info = localStorage.getItem("info");
+  const secondary = localStorage.getItem("secondary");
+  context.$vuetify.theme.themes.light.primary = primary ? primary : "#1d1d1d";
+  context.$vuetify.theme.themes.light.success = success ? success : "#00b894";
+  context.$vuetify.theme.themes.light.error = error ? error : "#f14668";
+  context.$vuetify.theme.themes.light.warning = warning ? warning : "#ffe08a";
+  context.$vuetify.theme.themes.light.accent = accent ? accent : "#485fc7";
+  context.$vuetify.theme.themes.light.info = info ? info : "#39b54a";
+  context.$vuetify.theme.themes.light.secondary = secondary
+    ? secondary
+    : "#ededed";
+  //console.log(theme);
   context.$axios
     .$get(context.env.config.BASE_URL + "/setting")
     .then(res => {
